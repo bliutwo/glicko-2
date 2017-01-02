4230bc948e260ea6180d4087adaf9c28d9c7f709
bliutwo <ossicle2@gmail.com>
2017-01-01 16:25:59 -0800

    Make progress on parsing individual matches/ids

--- make_matches_from_challonge.py
+++ make_matches_from_challonge.py
@@ -16,8 +16,22 @@ def get_info(username, api_key, url, info_str, t_str):
     print "sizeof " + info_str + "_data: " + str(len(l))
     return l
 
+# arguments:
+# m: matches_str, this is a string that contains the contents of the matches json
+# i: ids_str, this is a string that contains the contents of the ids json
 def parse_matches_ids_strs(m, i):
     pairs = []
+    m_list = m.split("}}")
+    i_list = i.split("}}")
+    for item in m_list:
+        print item
+        print
+    print
+    for item in i_list:
+        print item
+        print
+    assert(False)
+    pairs.append((m, i))
     return pairs
 
 # returns a list of tuples (winner, loser)
@@ -39,10 +53,10 @@ def main(argv):
         match_pairs = get_challonge_matches(argv[1], argv[2], argv[3])
         filename_matches = parse_link(argv[3]) + '_matches.json'
         f_matches = open(filename_matches, 'w')
-        for pair in s:
+        for pair in match_pairs:
             line = pair[0] + " " + pair[1]
             f_matches.write(line)
-            f.write('\n')
+            f_matches.write('\n')
         f_matches.close()
     else:
         print "Usage: python make_matches_from_challonge.py [username] [api-key] [url]"
